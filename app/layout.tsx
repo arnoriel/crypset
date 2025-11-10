40fea2a530efd313e10bb38b67bdfd55f428430f
arnoriel <azrilluthfimulyadi@gmail.com>
2025-11-10 16:49:39 +0700

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Crypto Finance Dashboard",
+  title: "Crypset",
   description: "A modern crypto market dashboard inspired by Google Finance but better.",
 };
 
